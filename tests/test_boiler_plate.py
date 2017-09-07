8217ac26f8187ad96794fdb2f0142d3b3c5729a5
Xinsheng Fang <xinsfang@cisco.com>
2017-09-07 10:42:46 +0000

    Add hello world

--- tests/test_boiler_plate.py
+++ tests/test_boiler_plate.py
@@ -11,15 +11,19 @@ Tests for `boiler_plate` module.
 import unittest
 
 import boiler_plate
+from boiler_plate.cli import hello
 
 
 class TestBoiler_plate(unittest.TestCase):
 
     def setUp(self):
+        self.hello_message = "Hello, Fang"
         pass
 
     def test_something(self):
         assert(boiler_plate.__version__)
+        output = hello()
+        assert(output == self.hello_message)
 
     def tearDown(self):
         pass
